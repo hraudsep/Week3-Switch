cfdd5cc08c4ae246d979dd0d161136129d2e675e
Helen Raudsepp <helen.raudsepp@k-print.ee>
2022-02-09 18:35:03 +0200

    add new projects

---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai;
+            //kui kasutaja sai A, konsool kuvab "Suurepärane!";
+            //kui B, konsool kuvab "Väga hea!";
+            //kui C, konsool kuvab "Hea!";
+            //kui D, konsool kuvab "Rahuldav!";
+            //kui E, konsool kuvab "Kasin!";
+            //kui F, konsool kuvab "Puudulik!;
+            //kui kasutaja sisestab midagi muud, siis konsool kuvab "Vale väärtus.";
+
+            //kasutame SWITCH
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (userResult)
+            {
+                case 'A':
+                    Console.WriteLine("Suurepärane!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Väga hea!");
+                    break;
+                case 'C':
+                    Console.WriteLine("Hea!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Rahuldav!");
+                    break;
+                case 'E':
+                    Console.WriteLine("Kasin!");
+                    break;
+                case 'F':
+                    Console.WriteLine("Puudulik!");
+                    break;
+                default:
+                    Console.WriteLine("Vale väärtus.");
+                    break;
+                    
+            }
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kastajal sisestada tema lemmikvärv;
+            //kui kasutaja sisestab "punane", konsool kuvab "Oled romantiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "Oled töökas";
+            //kui kasutaja sisestab "roheline", konsool kuvab "Oled loodusesõber";
+            //kui kasutaja sisestab midagi muud, konsool kuvab "Oled {userColor} ükssarvik";
+            Console.WriteLine("Sisesta oma lemmikvärv:");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch (userColor)
+            {
+                case "punane": //if(userColor == "punane")
+                    Console.WriteLine("Oled romantiline.");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled loodusesõber.");
+                    break;
+
+                default:
+                    Console.WriteLine($"Oled {userColor} ükssarvik.");
+                    break;
+            }
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
